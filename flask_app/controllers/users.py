017231a181e6e32cbd43dbc08538a053b2f943e9
Schneider-Alex <alexmanuelschneider@gmail.com>
2022-05-20 10:30:55 -0400

    creating page structure

--- flask_app/controllers/users.py
+++ flask_app/controllers/users.py
@@ -6,3 +6,15 @@ from flask_app.models import user
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/projects')
+def index():
+    return render_template('projects.html')
+
+@app.route('/resume')
+def index():
+    return render_template('resume.html')
+
+@app.route('/connect')
+def index():
+    return render_template('connect.html')
